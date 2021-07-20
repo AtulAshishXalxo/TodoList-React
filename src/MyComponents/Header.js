c0e56bf556d15bfb6fc23657ba6df862243782a6
AtulAshishXalxo <73930251+AtulAshishXalxo@users.noreply.github.com>
2021-07-20 14:06:01 +0530

    Update Header.js

--- src/MyComponents/Header.js
+++ src/MyComponents/Header.js
@@ -9,7 +9,7 @@ export default function Header(props) {
         
         <div className="md-6 mt-md-0 mt-3">
                 <nav className="navbar navbar-expand-lg navbar-dark bg-dark ">
-                    <Link className="navbar-brand mx-5  my-2" to="TodoList-React/"><strong>{props.title}</strong></Link>
+                    <Link className="navbar-brand mx-5  my-2" to="/TodoList-React/"><strong>{props.title}</strong></Link>
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
@@ -18,10 +18,10 @@ export default function Header(props) {
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav mr-auto">
                         <li className="nav-item active">
-                            <Link className="nav-link" to="TodoList-React/">Home</Link>
+                            <Link className="nav-link" to="/TodoList-React/">Home</Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="TodoList-React/about">About</Link>
+                            <Link className="nav-link" to="/TodoList-React/about">About</Link>
                         </li>
                         </ul>
                     </div>
